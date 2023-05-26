d5bc6f07f8350154d4881f68f7034dae4b1dc7c5
Giselle-nana <126684316+Giselle-nana@users.noreply.github.com>
2023-05-26 12:09:29 +0800

    Add files via upload
    能基本建表

---
+++ EvaluationSystem-5.26：12：08/EvaluationSystem/Models/User.cs
@@ -0,0 +1,31 @@
+﻿
+namespace EvaluationSystem.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Sex { get; set; }
+
+        //用户类型
+        public int Type { get; set; }
+
+        public User(string UserId,string Name,string Pwd,string Sex,int Type) 
+        {
+            this.UserId = UserId;
+            this.Name = Name;
+            this.Password = Pwd;
+            this.Sex = Sex;
+            this.Type = Type;
+        }
+        public User() 
+        {
+            
+        }
+
+        
+    }
+}
