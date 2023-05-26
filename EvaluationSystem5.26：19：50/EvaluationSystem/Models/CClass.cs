b37bd108ba08bda7a5a0db1b69cf64bcc38fd728
Giselle-nana <126684316+Giselle-nana@users.noreply.github.com>
2023-05-26 19:53:17 +0800

    Add files via upload

---
+++ EvaluationSystem5.26：19：50/EvaluationSystem/Models/CClass.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Transactions;
+
+namespace EvaluationSystem.Models
+{
+    public class CClass
+    {
+        [Key]
+        public string ClassId { get; set; }
+        public int ClassNum { get; set; }//班级号
+        public string  Grade { get; set; }//年级
+        public string  Academy { get; set; }//学院
+
+        public double AvaragePoint { get { return this.avaragePoint; } set { this.avaragePoint = value; } }//平均综测成绩
+        public int NotSubmitNum { get { return this.notSubmitNum; } 
+            set { if (value < 0) throw new ArgumentException("人数不能为负数！"); 
+                else this.notSubmitNum = value; } }//未提交人数
+
+        private int notSubmitNum;
+        private double avaragePoint;
+        public CClass()
+        {
+
+        }
+        public CClass(string id,int num,string cgrade,string cacademy) 
+        { 
+            this.ClassId = id;
+            this.ClassNum = num;
+            this.Grade = cgrade;
+            this.Academy = cacademy;
+        }
+
+    }
+}
---
+++ EvaluationSystem5.26：19：50/EvaluationSystem/Services/ComEvaService.cs
@@ -0,0 +1,19 @@
+﻿/*using EvaluationSystem.DAO;
+using EvaluationSystem.Models;
+
+namespace EvaluationSystem.Services
+{
+    public class ComEvaService
+    {
+        EvaDbContext DbContext;
+        public ComEvaService(EvaDbContext DbContext)
+        {
+            this.DbContext = DbContext;
+        }
+        public void AddComEvalution(ComEvaluation evaluation)
+        {
+            DbContext.ComEvaluations.Add(evaluation);
+            DbContext.SaveChanges();
+        }
+    }
+}*/
